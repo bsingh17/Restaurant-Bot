d10a720efcc0411fae0f310f4f721c8dbdc4661e
bsingh17 <61860973+bsingh17@users.noreply.github.com>
2020-07-28 10:41:17 +0530

    Update actions.py

--- src/actions.py
+++ src/actions.py
@@ -7,11 +7,15 @@ import pandas as pd
 
 dataset= pd.read_csv('dishes.csv')
 dataset=dataset.set_index('dish').T.to_dict('list')
+
+    
+
 class OrderForm(FormAction):
     """Collects order information"""
 
     def name(self):
         return "order_form"
+        
     @staticmethod
     def required_slots(tracker):
         return [
@@ -19,27 +23,64 @@ class OrderForm(FormAction):
             "mailid",
             "phone_number",
             "dish_name",
+            "continue",
             ]
+            
+    def validate_dish_name(self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        dish_name=tracker.get_slot("dish_name")
+        
+        if dish_name in dataset.keys(): 
+            
+            return {"dish_name":value}
+        else:
+            dispatcher.utter_template("utter_not_serving",tracker)
+            return {"dish_name":None}
     
+    
+    
+    
+    def validate_continue(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        dish_list=[]
+        if tracker.get_slot("continue")=="No":
+            dish_list.append(tracker.get_slot("dish_name"))
+            dish_name=tracker.get_slot("dish_name")
+            saveFile = open("some.txt", 'a')
+            saveFile.write(dish_name)
+            saveFile.close()
+            return {"continue":value}
+        else:
+            dish_list.append(tracker.get_slot("dish_name"))
+            return {"dish_name":None,"continue":None}
+            
+    
+        
     def submit(
         self,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict]:
+        
         username = tracker.get_slot("username")
         mailid = tracker.get_slot("mailid")
         phone_number=tracker.get_slot("phone_number")
         dish_name=tracker.get_slot("dish_name")
+        proceed=tracker.get_slot("continue")          
         
-        
-        if dish_name in dataset.keys(): 
-            message="ORDER DETAILS:"+"\n\n"+"Name:"+username+"\n"+"Email:"+mailid+"\n"+"Phone Number:"+phone_number+"\n"+"Dish Name:"+dish_name+"\nThanks for ordering! Your order will be placed soon." 
-            saveFile = open("some.txt", 'a')
-            saveFile.write(message)
-            saveFile.close()
-            dispatcher.utter_message(message)
-        else: 
-            message="Sorry currently we are not serving this dish."    
+        message="ORDER DETAILS:"+"\n\n"+"Name:"+username+"\n"+"Email:"+mailid+"\n"+"Phone Number:"+phone_number+"\n"+"\nThanks for ordering! Your order will be placed soon." 
+        saveFile = open("some.txt", 'a')
+        saveFile.write(message)
+        saveFile.close()
         dispatcher.utter_message(message)
         return []
